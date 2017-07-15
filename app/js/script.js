8223f1eb3fd2c92b2a2de42118806d742cf51772
slayerjk <slayer.jk@mail.ru>
2017-07-15 17:09:20 +0600

    All CSS - done; Slider - Done(CSS); Mobile menu js code - done; Responsive site - done

--- app/js/script.js
+++ app/js/script.js
@@ -4,7 +4,7 @@
 $(document).ready(function () {
   'use strict';
 
-/*event.preventDefault() gor IE9(event.returnValue = false)*/
+  /*event.preventDefault() gor IE9(event.returnValue = false)*/
   $.fn.eventPreventDefaultSafe = function () {
     if (event.preventDefault) {
       event.preventDefault();
@@ -12,8 +12,30 @@ $(document).ready(function () {
       event.returnValue = false;
     }
   };
-/*-------------------------------------------------------*/
+  /*-------------------------------------------------------*/
+  $(window).resize(function () {
+    var w = $(window).width();
+    if (w < 768) {
+      $('.main-nav__menu-toggle').show();
+      if (w < 768 && $('.main-nav__list').hide()) {
+        $('.main-nav__menu-toggle').removeClass('main-nav__menu-toggle--close');
+        $('.main-nav__list').hide();
+      } else if (w < 768 && $('.main-nav__list').show()) {
+        $('.main-nav__menu-toggle').addClass('main-nav__menu-toggle--close');
+        $('.main-nav__list').show();
+      }
+    } else if (w >= 768) {
+      $('.main-nav__list').show();
+      $('.main-nav__menu-toggle').hide();
+    }
+  });
 
+  $('.main-nav__menu-toggle').on('click', function () {
+    event.preventDefault();
+    
+    $('.main-nav__menu-toggle').toggleClass('main-nav__menu-toggle--close');
+    $('.main-nav__list').slideToggle('slow');
+  });
 
 });
 
